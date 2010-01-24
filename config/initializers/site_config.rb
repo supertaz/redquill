9e1a52ed2cf813f2b86bf2ad6df3fd63026f43e4
Jon Mischo <jmischo@quagility.com>
2010-01-23 20:31:18 -0600

    Cleaned up site config for safe release.

---
+++ config/initializers/site_config.rb
@@ -0,0 +1,17 @@
+#
+# For site defaults
+#
+DEFAULT_SITE_NAME = 'Test Blog'
+DEFAULT_TITLE = 'A Blog'
+DEFAULT_HOST = 'localhost:3000'
+
+#
+# For Google Analytics
+#
+WEB_PROPERTY_ID = 'UA-000000-1'
+ANALYTICS_DOMAIN = 'my.domain.tld'
+
+#
+# For shortened bit.ly links to posts
+#
+SiteBitly = Bitly.new('mysite', 'mykey')
